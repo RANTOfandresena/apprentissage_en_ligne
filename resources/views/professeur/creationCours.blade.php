ee70027106c398b3e4af51a89a63495a57f71ad4
Ranto <herimandrantofandresen@gmail.com>
2023-12-11 14:23:42 +0300

    --

--- resources/views/professeur/creationCours.blade.php
+++ resources/views/professeur/creationCours.blade.php
@@ -13,13 +13,15 @@
 
     <p> Contenu du cours id : </p>
 
-    @foreach ($matiere->contenu_du_cours as $contenu )
-        <p> {{ $contenu->id }} </p>
-    @endforeach
+
 
     <div class="niveau">
-        <div class="active"><a> niveau 1 </a></div>
-        <div><a> niveau 2 </a></div>
+        @foreach ($matiere->contenu_du_cours as $contenu )
+            <p> {{ $contenu->id }} </p>
+            <div class="active"><a> niveau 1 </a></div>
+            <div><a> niveau 2 </a></div>
+        @endforeach
+        
         <div style="width: 181px;"><a href="{{route('professeur.ajoutNiveau', ['matiere' => $matiere->id] )}}"> Ajouter un autre niveau  </a></div>
     </div>
     <div id="cours" data-id="{{6}}"></div>
