6dbee583b22a3baf65741521b6229637b5599a8c
Voni <angevoni@gmail.com>
2023-12-15 13:41:53 +0300

    Suite intégration maquette interface utilisateurs

--- resources/views/admin/navbarAdmin.blade.php
+++ resources/views/admin/navbarAdmin.blade.php
@@ -1,14 +1,38 @@
 @section('navbar')
 
+@php
+     $routeName = request()->route()->getName();
+
+@endphp
 <div class="partie1">
     <div class="navbarAdmin">
         <div class="fondNavbar">
             <div class="linkAdmin">
-                <a href="#">Accueil</a>
-                <a href="#">Cours</a>
-                <a href="#">Gestion compte utilisateur</a>
-                <a href="{{route('admin.professeur')}}">Informations professeurs</a>
-                <a href="#">Résultat des tests</a>
+                <a href="#"
+                    @if($routeName == 'admin.accueil')
+                        class="active"
+                    @endif>
+                Accueil</a>
+                <a href="#"
+                    @if($routeName == 'a')
+                        class="active"
+                    @endif
+                >Cours</a>
+                <a href="#"
+                    @if($routeName == 'b')
+                        class="active"
+                    @endif
+                >Gestion compte utilisateur</a>
+                <a href="{{route('admin.professeur')}}"
+                    @if($routeName == 'c')
+                        class="active"
+                    @endif
+                >Informations professeurs</a>
+                <a href="#"
+                    @if($routeName == 'd')
+                        class="active"
+                    @endif
+                >Résultat des tests</a>
                 <input type="search" name="" id="" placeholder="RECHERCHE" class="recherche_admin">
                 <form action="{{route('auth.logout')}}" method="post">
                     @csrf
@@ -27,19 +51,15 @@
     {{-- Décorations --}}
     <div class="rect1"></div>
     <div class="rect2"></div>
-    <div class="rect3"></div>
+    <div class="rect3">
+            {{-- Phrases --}}
+        <h1 class="ODATA"> ODATA-LEARNING </h1>
+        <h2 class="Connaissance"> <span class="Connaissance">CONNAISSANCE</span> <br> A PORTER DE MAIN</h2>
+    </div>
     <div class="rect4"></div>
     <div class="rect5"></div>
 
-    {{-- Phrases --}}
-    <h1 class="ODATA"> ODATA-LEARNING </h1>
-    <h2 class="Connaissance"> <span class="Connaissance">CONNAISSANCE</span> <br> A PORTER DE MAIN</h2>
-</div>
 
-<div class="partie2">
-    <p class="titrePart">Les cours récement crées</p>
-    <p class="PhraseAppel">Faîtes sortir leur potentiel</p>
-    <button class="Voir_plus"> Voir plus </button>
 </div>
 
 @endsection
